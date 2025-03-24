c9f8456786b5fa672e01e3b277c49c54a085bfb3
Bárbara Stella <barbarastellaw@gmail.com>
2025-03-24 13:22:47 -0300

    Adição de informações sobre o evento. Páginas Sobre, Programação, Arena Games, Trabalhos

--- src/schedule.js
+++ src/schedule.js
@@ -1,10 +1,82 @@
 const scheduleData = {
-    '28': [
+    '11': [
         {
-            title: 'Palestra de abertura',
+            title: 'Palestra de apoiadores',
+            date: '11/08/2025',
             time: '08:00 às 12:00',
             location: 'A definir',
             description: 'Cerimônia de abertura do evento.',
+            /*speakers: [{
+               name: 'Rafael Rieder',
+               image: '../../assets/img/speakers/Rieder.jpg',
+               titles: ['Doutor e pesquisador em Ciência da Computação,'],
+               bio: 'Rafael Rieder é pesquisador e doutor em Ciência da Computação, docente permanente do Programa de Pós-Graduação em Computação Aplicada (PPGCA), e professor da Universidade de Passo Fundo (UPF) desde 2011. Participou da comissão de criação do curso de Engenharia de Computação na UPF, e foi o primeiro coordenador do curso (2014-2015). Também atuou na comissão de criação do PPGCA na UPF (2013), e foi coordenador do programa no quadriênio 2017-2020. Publicou vários artigos em periódicos qualificados e trabalhos em anais de conferências nacionais e internacionais, além de organizar eventos dentro das temáticas de Realidade Virtual e Aumentada e Computação Aplicada à Agricultura. Desde 2016, coordena o Núcleo de Visualização e Modelagem Computacional no Parque Científico e Tecnológico da UPF (UPF Parque), onde está sediado o Laboratório de Realidade Virtual e Computação de Alto Desempenho. Seus principais projetos de P&D&I têm interface de Computação Aplicada à Agricultura e à Saúde, nas áreas de Realidade Virtual, Realidade Aumentada, Realidade Misturada, Aprendizado de Máquina, Processamento de Imagens e Visão Computacional. É bolsista produtividade em desenvolvimento tecnológico e extensão inovadora do CNPq - Nível 2.'
+           }]*/
+        }
+    ],
+    '12': [
+        {
+            title: 'Workshop 1',
+            date: '12/08/2025',
+            time: '08:00 às 12:00',
+            location: 'A definir',
+            description: 'Atividade sobre IA.',
+            /*speakers: [{
+               name: 'Rafael Rieder',
+               image: '../../assets/img/speakers/Rieder.jpg',
+               titles: ['Doutor e pesquisador em Ciência da Computação,'],
+               bio: 'Rafael Rieder é pesquisador e doutor em Ciência da Computação, docente permanente do Programa de Pós-Graduação em Computação Aplicada (PPGCA), e professor da Universidade de Passo Fundo (UPF) desde 2011. Participou da comissão de criação do curso de Engenharia de Computação na UPF, e foi o primeiro coordenador do curso (2014-2015). Também atuou na comissão de criação do PPGCA na UPF (2013), e foi coordenador do programa no quadriênio 2017-2020. Publicou vários artigos em periódicos qualificados e trabalhos em anais de conferências nacionais e internacionais, além de organizar eventos dentro das temáticas de Realidade Virtual e Aumentada e Computação Aplicada à Agricultura. Desde 2016, coordena o Núcleo de Visualização e Modelagem Computacional no Parque Científico e Tecnológico da UPF (UPF Parque), onde está sediado o Laboratório de Realidade Virtual e Computação de Alto Desempenho. Seus principais projetos de P&D&I têm interface de Computação Aplicada à Agricultura e à Saúde, nas áreas de Realidade Virtual, Realidade Aumentada, Realidade Misturada, Aprendizado de Máquina, Processamento de Imagens e Visão Computacional. É bolsista produtividade em desenvolvimento tecnológico e extensão inovadora do CNPq - Nível 2.'
+           }]*/
+        },
+        {
+            title: 'Oficina 1',
+            date: '12/08/2025',
+            time: '08:00 às 12:00',
+            location: 'A definir',
+            description: 'Atividade interativa sobre IA.',
+            /*speakers: [{
+               name: 'Rafael Rieder',
+               image: '../../assets/img/speakers/Rieder.jpg',
+               titles: ['Doutor e pesquisador em Ciência da Computação,'],
+               bio: 'Rafael Rieder é pesquisador e doutor em Ciência da Computação, docente permanente do Programa de Pós-Graduação em Computação Aplicada (PPGCA), e professor da Universidade de Passo Fundo (UPF) desde 2011. Participou da comissão de criação do curso de Engenharia de Computação na UPF, e foi o primeiro coordenador do curso (2014-2015). Também atuou na comissão de criação do PPGCA na UPF (2013), e foi coordenador do programa no quadriênio 2017-2020. Publicou vários artigos em periódicos qualificados e trabalhos em anais de conferências nacionais e internacionais, além de organizar eventos dentro das temáticas de Realidade Virtual e Aumentada e Computação Aplicada à Agricultura. Desde 2016, coordena o Núcleo de Visualização e Modelagem Computacional no Parque Científico e Tecnológico da UPF (UPF Parque), onde está sediado o Laboratório de Realidade Virtual e Computação de Alto Desempenho. Seus principais projetos de P&D&I têm interface de Computação Aplicada à Agricultura e à Saúde, nas áreas de Realidade Virtual, Realidade Aumentada, Realidade Misturada, Aprendizado de Máquina, Processamento de Imagens e Visão Computacional. É bolsista produtividade em desenvolvimento tecnológico e extensão inovadora do CNPq - Nível 2.'
+           }]*/
+        },
+    ],
+    '13': [
+        {
+            title: 'Oficina 2',
+            date: '13/08/2025',
+            time: '08:00 às 10:00',
+            location: 'A definir',
+            description: 'Atividade interativa sobre IA.',
+            /*speakers: [{
+               name: 'Rafael Rieder',
+               image: '../../assets/img/speakers/Rieder.jpg',
+               titles: ['Doutor e pesquisador em Ciência da Computação,'],
+               bio: 'Rafael Rieder é pesquisador e doutor em Ciência da Computação, docente permanente do Programa de Pós-Graduação em Computação Aplicada (PPGCA), e professor da Universidade de Passo Fundo (UPF) desde 2011. Participou da comissão de criação do curso de Engenharia de Computação na UPF, e foi o primeiro coordenador do curso (2014-2015). Também atuou na comissão de criação do PPGCA na UPF (2013), e foi coordenador do programa no quadriênio 2017-2020. Publicou vários artigos em periódicos qualificados e trabalhos em anais de conferências nacionais e internacionais, além de organizar eventos dentro das temáticas de Realidade Virtual e Aumentada e Computação Aplicada à Agricultura. Desde 2016, coordena o Núcleo de Visualização e Modelagem Computacional no Parque Científico e Tecnológico da UPF (UPF Parque), onde está sediado o Laboratório de Realidade Virtual e Computação de Alto Desempenho. Seus principais projetos de P&D&I têm interface de Computação Aplicada à Agricultura e à Saúde, nas áreas de Realidade Virtual, Realidade Aumentada, Realidade Misturada, Aprendizado de Máquina, Processamento de Imagens e Visão Computacional. É bolsista produtividade em desenvolvimento tecnológico e extensão inovadora do CNPq - Nível 2.'
+           }]*/
+        },
+        {
+            title: 'Hackathon',
+            date: '13/08/2025',
+            time: '08:00 às 12:00',
+            location: 'A definir',
+            description: 'Hackathons são eventos que reúnem desenvolvedores de software, designers e demais  profissionais relacionados à área de programação, com o intuito de criarem soluções inovadoras para algum problema específico',
+            speakers: [
+                {
+                    name: '⁠Murilo Bertella Ossanes',
+                    link: 'https://wa.link/lswjv7',
+                    image: '../../assets/img/Murilo.jpg'
+                }]
+        },
+    ],
+    '28': [
+        {
+            title: 'Atividade com apoiadores: Palestra',
+            date: '28/05/2025',
+            time: '08:00 às 12:00',
+            location: 'A definir',
+            description: 'Evento de divulgação do evento e do patrocinador.',
             /*speakers: [{
                 name: 'Rafael Rieder',
                 image: '../../assets/img/speakers/Rieder.jpg',
@@ -16,20 +88,28 @@ const scheduleData = {
     '31': [
         {
             title: 'Arena Games (Counter Strike e League of Legends)',
+            date: '31/05/2025',
             time: '08:00 às 11:00',
             location: 'Prédio a definir',
             description: 'Competição de jogos eletrônicos: Counter Strike e League of Legends. Aberto ao público, inscrições através do Painel. Traga seus amigos!',
             link: 'https://painel.passofundo.ifsul.edu.br/',
-            /* speakers: [
-                 {
-                     name: 'Pedro Eduardo Camera',
-                     image: '../../assets/img/speakers/Unknown.jpg'
-                 },
-                 {
-                     name: 'João Víctor Alves da Cruz',
-                     image: '../../assets/img/speakers/Unknown.jpg'
-                 }
-             ]*/
+            speakers: [
+                {
+                    name: 'Marjory Ane Toazza',
+                    link: 'https://wa.link/03ovmc',
+                    image: '../../assets/img/Marjory.jpg'
+                },
+                {
+                    name: '⁠Murilo Bertella Ossanes',
+                    link: 'https://wa.link/lswjv7',
+                    image: '../../assets/img/Murilo.jpg'
+                },
+                {
+                    name: '⁠Victor do Amarante dos Santos',
+                    link: 'https://wa.link/9zoeno',
+                    image: '../../assets/img/Victor.jpg'
+                }
+            ]
         },
     ],
 };
@@ -45,6 +125,7 @@ function createSpeakersSection(speakers) {
                         ${speaker.titles.map(title => `<p>${title}</p>`).join('')}
                     </div>
                 ` : ''}
+                <a href="${speaker.link}" target="_blank">Clique para contato</a>
             </div>
         </div>
     `).join('');
@@ -70,7 +151,6 @@ function createEventCard(event, index) {
             <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#eventModal${index}">
                 Mais informações</button>
             
-            <!-- Modal -->
             <div class="modal fade" id="eventModal${index}" tabindex="-1" aria-labelledby="eventModalLabel${index}" aria-hidden="true">
                 <div class="modal-dialog modal-lg">
                     <div class="modal-content">
@@ -80,13 +160,15 @@ function createEventCard(event, index) {
                         </div>
                         <div class="modal-body">
                             <div class="event-details">
+                            <p><strong>Data:</strong> ${event.date}</p>
                                 <p><strong>Horário:</strong> ${event.time}</p>
                                 <p><strong>Local:</strong> ${event.location}</p>
                                 ${event.description ? `<p><strong>Descrição:</strong> ${event.description}</p>` : ''}
                             </div>
+                            <p><strong>Inscrições:</strong> <a href="${event.link}" target="_blank">${event.link}</a></p>
                             ${event.speakers ? `
                                 <div class="speakers-details">
-                                    <h6>Palestrante(s)</h6>
+                                    <h6>Organizadores</h6>
                                     ${event.speakers.map(speaker => `
                                         <div class="speaker-detail">
                                             <img src="${speaker.image}" alt="${speaker.name}" class="speaker-image">
@@ -125,6 +207,85 @@ function updateEventsList(day) {
 
 document.addEventListener('DOMContentLoaded', () => {
     const dayButtons = document.querySelectorAll('.day-button');
+    const eventsContainer = document.querySelector('.events-list');
+
+    function updateEventsList(day) {
+        const events = scheduleData[day];
+        if (events) {
+            eventsContainer.innerHTML = events.map((event, index) => createEventCard(event, index, day)).join('');
+        }
+    }
+
+    function createEventCard(event, index, day) {
+        return `
+            <div class="event-card">
+                <h3 class="event-title">${event.title}</h3>
+                <div class="event-date">
+                    <i class="bi bi-calendar-check"></i>
+                    <span>${event.date}</span>
+                </div>
+                <div class="event-time">
+                    <i class="bi bi-clock"></i>
+                    <span>${event.time}</span>
+                </div>
+                <div class="event-location">
+                    <i class="bi bi-geo-alt"></i>
+                    <span>${event.location}</span>
+                </div>
+                ${event.speakers ? `
+                    <div class="speakers-container">
+                        ${createSpeakersSection(event.speakers)}
+                    </div>
+                ` : ''}
+                ${day === '31' ? `
+                    <a href="../arenagame/index.html" class="btn btn-primary">Mais informações</a>
+                ` : `
+                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#eventModal${index}">
+                        Mais informações
+                    </button>
+                    <div class="modal fade" id="eventModal${index}" tabindex="-1" aria-labelledby="eventModalLabel${index}" aria-hidden="true">
+                        <div class="modal-dialog modal-lg">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="eventModalLabel${index}">${event.title}</h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    <div class="event-details">
+                                        <p><strong>Horário:</strong> ${event.time}</p>
+                                        <p><strong>Local:</strong> ${event.location}</p>
+                                        <p><strong>Descrição:</strong> ${event.description}</p>
+                                    </div>
+                                    ${event.speakers ? `
+                                        <div class="speakers-details">
+                                            <h6>Organizadores</h6>
+                                            ${event.speakers.map(speaker => `
+                                                <div class="speaker-detail">
+                                                    <img src="${speaker.image}" alt="${speaker.name}" class="speaker-image">
+                                                    <div class="speaker-info">
+                                                        <h4>${speaker.name}</h4>
+                                                        ${speaker.titles ? `
+                                                            <div class="speaker-titles">
+                                                                ${speaker.titles.map(title => `<p>${title}</p>`).join('')}
+                                                            </div>
+                                                        ` : ''}
+                                                        ${speaker.bio ? `<p class="speaker-bio">${speaker.bio}</p>` : ''}
+                                                    </div>
+                                                </div>
+                                            `).join('')}
+                                        </div>
+                                    ` : ''}
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                `}
+            </div>
+        `;
+    }
 
     dayButtons.forEach(button => {
         button.addEventListener('click', () => {
@@ -133,5 +294,7 @@ document.addEventListener('DOMContentLoaded', () => {
             updateEventsList(button.textContent);
         });
     });
-    updateEventsList('15');
+
+    updateEventsList('31'); // alterar para 28
+    document.querySelector(".day-button[data-day='28']").classList.add('active');
 });
